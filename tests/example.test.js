326fa8512f332f87de1bb577b3851e51b13bbb0f
Moe Farid <moe.faridd@gmail.com>
2021-04-01 19:38:48 -0400

    2

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,9 +2,17 @@ const puppeteer = require('puppeteer')
 
 describe('My First Puppeteer Test', () => {
 	it('Should launch the browser', async function () {
-		const browser = await puppeteer.launch({ headless: false })
+		const browser = await puppeteer.launch({
+			headless: false,
+			slowMo: 10,
+			devtools: false,
+		})
 		const page = await browser.newPage()
 		await page.goto('http://example.com/')
+		await page.waitForTimeout(3000)
+		await page.waitForSelector('h1')
+		await page.reload()
+		await page.waitForTimeout(3000)
 		await page.waitForSelector('h1')
 		await browser.close()
 	})
