14a7ce178601ffdc46e448b2860392bf406e271a
Moe Farid <moe.faridd@gmail.com>
2021-04-01 21:33:17 -0400

    2

--- tests/example.test.js
+++ tests/example.test.js
@@ -4,16 +4,18 @@ describe('My First Puppeteer Test', () => {
 	it('Should launch the browser', async function () {
 		const browser = await puppeteer.launch({
 			headless: false,
-			slowMo: 10,
+			slowMo: 250,
 			devtools: false,
 		})
 		const page = await browser.newPage()
-		await page.goto('http://example.com/')
-		await page.waitForTimeout(3000)
-		await page.waitForSelector('h1')
-		await page.reload()
-		await page.waitForTimeout(3000)
-		await page.waitForSelector('h1')
+		await page.goto('https://devexpress.github.io/testcafe/example/')
+		await page.type('#developer-name', 'Mike', { delay: 0 })
+		await page.click('#populate', { clickCount: 1 })
+		await page.waitForTimeout(2000)
+
+		await page.click('#tried-test-cafe', { clickCount: 1 })
+		await page.select('#preferred-interface', 'JavaScript API')
+		await page.waitForTimeout(2000)
 		await browser.close()
 	})
 })
