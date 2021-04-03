0332730e94ede42a240cd1e68606fe19cff5e62e
Moe Farid <moe.faridd@gmail.com>
2021-04-02 23:47:54 -0400

    4

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,3 +1,4 @@
+const { expect } = require('chai')
 const puppeteer = require('puppeteer')
 
 describe('My First Puppeteer Test', () => {
@@ -8,15 +9,24 @@ describe('My First Puppeteer Test', () => {
 			devtools: false,
 		})
 		const page = await browser.newPage()
-		await page.goto('https://devexpress.github.io/testcafe/example/')
-		await page.type('#developer-name', 'Mike', { delay: 0 })
-		await page.click('#tried-test-cafe', { clickCount: 1 })
-		await page.select('#preferred-interface', 'JavaScript API')
-		const message = 'lets fill it up with bunch of txt'
-		await page.type('#comments', message)
-		await page.click('#submit-button')
-		await page.waitForSelector('.result-content')
-		await page.waitForTimeout(5000)
+		await page.setDefaultTimeout(10000)
+		await page.setDefaultNavigationTimeout(20000)
+		await page.goto('https://dev.to')
+
+		await page.goto('https://example.com/')
+		const title = await page.title()
+		const url = await page.url()
+		const text = await page.$eval('h1', element => element.textContent)
+		const count = await page.$$eval('p', element => element.length)
+		const parags = await page.$eval('p', element => element.textContent)
+		console.log('Text in h1 is: ' + text)
+		console.log('Count of paragraphs :' + count)
+		console.log('All paragraphs are: ' + parags)
+
+		expect(title).to.be.a('string', 'Example Domain')
+		expect(url).to.include('example.com')
+		expect(text).to.be.a('string', 'Example Domain')
+		expect(count).to.equal(2)
 		await browser.close()
 	})
 })
